41bb0321ae9d340efcf38cbeb83b468561d14af8
Alexander Londoño Espejo <alexander.londono@secuencia24.com>
2016-06-09 15:16:58 -0500

    Consuming services POST task √

--- routes/index.js
+++ routes/index.js
@@ -12,12 +12,22 @@ var mongoose = require('mongoose');
 var Tasks = mongoose.model('Tasks');
 
 //GET - List Tasks
-router.get('/tasks'), function(req, res, next){
+router.get('/tasks', function(req, res, next){
   Tasks.find(function(err, tasks){
     if(err){return next(err)}
 
     res.json(tasks);
   })
-}
+})
+
+//POST - Add Task
+router.post('/task', function(req, res, next){
+  var task = new Tasks(req.body);
+
+  task.save(function(err, task){
+    if(err){return next(err)}
+    res.json(task);
+  })
+})
 
 module.exports = router;
